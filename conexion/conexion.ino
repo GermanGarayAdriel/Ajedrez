7aaf37ec7b49a69be237c99eddb7b97d91ec8e2e
AleVZ <a.schwartzmann@hotmail.com>
2017-09-19 17:38:40 -0300

    Agregado el archivo conexion funcional

--- conexion/conexion.ino
+++ conexion/conexion.ino
@@ -1,16 +1,11 @@
 #include <SoftwareSerial.h>
-#include <WiFi.h>
-const char* ssid = "G6_9776";
-const char* password = "12345678";
  
 SoftwareSerial esp8266(3,2); // RX||TX
 void setup()
 {
   Serial.begin(57600);
   esp8266.begin(57600);
-  WiFi.begin(ssid, password);//AT+CWJAP:"SSID","PASSWD"
-  Serial.println("WiFi connected");
-  Serial.println(WiFi.localIP());
+  Wifi.config("192.168.0.2","192.168.0.1","192.168.0.1","255.255.255.252")
 }
  void loop()
 {
